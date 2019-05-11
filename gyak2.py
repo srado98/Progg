ddb421f9f56b0f5ceddeafb989fb105e5761473b
srado98 <37191034+srado98@users.noreply.github.com>
2019-05-11 11:26:50 +0200

    Add files via upload

---
+++ gyak2.py
@@ -0,0 +1,29 @@
+import numpy as np
+import random
+import sys
+
+
+n1=int(sys.argv[1])
+n2=int(sys.argv[2])
+n3=int(sys.argv[3])
+n4=int(sys.argv[4])
+
+
+a=np.random.random((n1,n2));
+b=np.random.random((n3,n4));
+s=(n1,n4)
+szorzat=np.zeros(s)
+if n2!=n3:
+    print("A két mátrix nem szorozható össze")
+
+for i in range(len(a)):
+    for j in range(len(b[0])):
+        for k in range(len(b)):
+            szorzat[i][j]+=a[i][k]*b[k][j]
+
+
+print("A matrix:\n",a)
+print("B mátrix:\n",b)
+print("A szorzatuk:\n",szorzat)
+
+
